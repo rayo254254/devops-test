e1f24da629d18b61696210ce3b5a781eea9a708e
rayo254254 <r1019767@student.thomasmore.be>
2025-07-24 14:46:30 +0200

    test30

--- omondi.py
+++ omondi.py
@@ -8,3 +8,6 @@ def hello():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
+    print("This is the main module of ryan.py")
+else:
+    print("This is not the main module of ryan.py")
