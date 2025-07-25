0a4e527b564d0618bed095b20f25b6cc00fb523f
rayo254254 <r1019767@student.thomasmore.be>
2025-07-25 14:49:49 +0200

    GCR2

--- testing deployment/main.py
+++ testing deployment/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, this is the home page!"
+    return "Hello, this is the test on local server!"
 
 if __name__ == "__main__":
     app.run(debug=True)
